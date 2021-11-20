87c0146dd1213fa573c07be57afb7e0e719108cd
Antony Raj <antonyraj@Antonys-MacBook-Air.local>
2021-11-20 21:01:29 +0530

    Timer: Add pre digit below 10

--- src/shared/components/Timer/index.tsx
+++ src/shared/components/Timer/index.tsx
@@ -5,10 +5,15 @@ import stopwatch from "../../../assets/icons/stopwatch.svg";
 const Time = () => {
   const { seconds, minutes, hours } = useStopwatch({ autoStart: true });
 
+  const with2Places = (value: number) => value.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  })
+
   return (
     <p>
-      <img src={stopwatch} alt="clock"/>{hours ? hours + ":" : ""}
-      {minutes}:{seconds}
+      <img src={stopwatch} alt="clock"/>{hours ? with2Places(hours) + ":" : ""}
+      {with2Places(minutes)}:{with2Places(seconds)}
     </p>
   );
 };
